b5ba4b8600762716ae4533a82f9efe1d96e94671
Truong Luu <dinhluu1259@gmail.com>
2026-02-20 10:44:14 +0700

    feat(server): configure CORS with credentials support

--- src/server.js
+++ src/server.js
@@ -1,28 +1,30 @@
-import express from "express"; 
+import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./libs/db.js";
 import authRoute from "./routes/authRoute.js";
 import userRoute from "./routes/userRoute.js";
 import cookieParser from "cookie-parser";
 import { protectedRoute } from "./middlewares/authMiddlewares.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-// Middlewares    middlewares là những hàm chạy trước khi vào logic chính của API   
+// Middlewares    middlewares là những hàm chạy trước khi vào logic chính của API
 app.use(express.json());
-app.use(cookieParser()); 
+app.use(cookieParser());
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 //public routes
-app.use('/api/auth', authRoute);
+app.use("/api/auth", authRoute);
 
 //private routes
-app.use(protectedRoute)
-app.use('/api/user', userRoute);
+app.use(protectedRoute);
+app.use("/api/user", userRoute);
 
 connectDB().then(() => {
-    app.listen(PORT, () => {
+  app.listen(PORT, () => {
     console.log(`server bắt đầu trên cổng ${PORT}`);
-    });
+  });
 });
